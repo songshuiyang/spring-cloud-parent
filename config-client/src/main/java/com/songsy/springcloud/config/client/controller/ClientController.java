2326a9f2e66987c3c6127d030fc54668c45a2021
songshuiyang <1459074711@qq.com>
2018-10-25 23:07:47 +0800

    更新Config client模块

--- config-client/src/main/java/com/songsy/springcloud/config/client/controller/ClientController.java
+++ config-client/src/main/java/com/songsy/springcloud/config/client/controller/ClientController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RefreshScope
 public class ClientController {
 
-//    @Value("${name}")
+    @Value("${name}")
     private String name;
 
     @GetMapping
